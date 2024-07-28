f598dc8af6859e2fd88a351abc9e772fa033a11a
Rachell Aristo <123459774+sleepycodingcat@users.noreply.github.com>
2024-07-28 14:22:08 +0800

    Update file paths

--- cat counter index.js
+++ cat counter index.js
@@ -10,7 +10,7 @@ function newCat(){
     let ranNum = Math.floor(Math.random() * catImg.length);
     console.log(ranNum);
     let img = new Image();
-    img.src = catImg[ranNum];
+    img.src = "media/"+catImg[ranNum];
     img.style.width = "10vw";
     document.getElementById('cats').appendChild(img);
 }
@@ -19,7 +19,7 @@ function newMeow(){
     let ranNum = Math.floor(Math.random() * 3 +1);
     console.log(ranNum);
     let audio = new Audio();
-    audio.src = `meow${ranNum}.mp3`;
+    audio.src = `media/meow${ranNum}.mp3`;
     audio.loop = true;
     audio.autoplay = true;
     document.getElementById("meow").appendChild(audio);
